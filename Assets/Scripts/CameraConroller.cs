0f60c22b7c57efdb9ee0ce9c37ad9d43a9fa2456
Александр Подвысоцкий <alexanderpodvisotskyi@gmail.com>
2019-01-18 23:17:26 +0200

    Add work with camera (now can not go beyond the boundaries of the map) but work with camera is not finished

--- Assets/Scripts/CameraConroller.cs
+++ Assets/Scripts/CameraConroller.cs
@@ -1,20 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class CameraConroller : MonoBehaviour
 {
 
     public Transform target;
+
+    public Tilemap theMap;
+
+    private Vector3 bottomLeftLimit;
+    private Vector3 topRigthLimit;
+
+    private float halfHeigth;
+    private float halfWidth;
+
+
     // Start is called before the first frame update
     void Start()
     {
         target = PlayerController.instense.transform;
+
+        halfHeigth = Camera.main.orthographicSize;
+        halfWidth = halfHeigth * Camera.main.aspect;
+
+        bottomLeftLimit = theMap.localBounds.min + new Vector3(halfWidth, halfHeigth, 0f);
+        topRigthLimit = theMap.localBounds.max + new Vector3(-halfWidth, -halfHeigth, 0f);
     }
 
     // LateUpdate is called once per frame after Update
     void LateUpdate() 
     {
         transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
+
+        // Keep camera inside the bounds
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, bottomLeftLimit.x, topRigthLimit.x), Mathf.Clamp(transform.position.y, bottomLeftLimit.y, topRigthLimit.y), transform.position.z);
     }
 }
