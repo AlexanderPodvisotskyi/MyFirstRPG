54c34a10f34a2ed9e4cd5bfe6744ff204df09d4f
AlexanderPK <alexanderpodvisotskyi@gmail.com>
2019-03-19 23:35:17 +0200

    Add Dialog manager and Dialog window

---
+++ Assets/Scripts/DialogManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogManager : MonoBehaviour
+{
+	public Text dialogText;
+	public Text nameNPSText;
+
+	public GameObject DialogBox;
+	public GameObject NPSBox;
+
+	public string[] dialogLinesArray;
+
+	public int currentLine;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		dialogText.text = dialogLinesArray[currentLine];
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (DialogBox.activeInHierarchy)
+		{
+			if (Input.GetButtonUp("EnterDialog"))
+			{
+				currentLine++;
+
+				if (currentLine >= dialogLinesArray.Length)
+				{
+					DialogBox.SetActive(false);
+				}
+				else
+				{
+					dialogText.text = dialogLinesArray[currentLine];
+				}
+			}
+		}
+
+	}
+}
