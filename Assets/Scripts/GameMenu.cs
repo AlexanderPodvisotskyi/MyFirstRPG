240402b38f882bd5167263920d44468e503b7210
AlexanderPK <alexanderpodvisotskyi@gmail.com>
2019-04-02 23:31:21 +0300

    Create gameMenu (Button G in Game)

---
+++ Assets/Scripts/GameMenu.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class GameMenu : MonoBehaviour
+{
+	public GameObject theMenu;
+	// Start is called before the first frame update
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetButtonDown("OpenMenu"))
+		{
+			if (theMenu.activeInHierarchy)
+			{
+				theMenu.SetActive(false);
+			} 
+			else
+			{
+				theMenu.SetActive(true);
+			}
+		}
+
+	}
+}
