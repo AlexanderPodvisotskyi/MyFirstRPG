5abb3d1c00f5c6c89e709e7af07e0dbf4923f069
AlexanderPK <alexanderpodvisotskyi@gmail.com>
2019-04-07 18:14:54 +0300

    Add work with menu and add stats players in window.

--- Assets/Scripts/GameMenu.cs
+++ Assets/Scripts/GameMenu.cs
@@ -1,8 +1,22 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class GameMenu : MonoBehaviour
 {
 	public GameObject theMenu;
+	public GameObject[] windows;
+
+	public Text[] nameTextArray;
+	public Text[] hpTextArray;
+	public Text[] mpTextArray;
+	public Text[] lvlTextArray;
+	public Image[] personalImage;
+
+	public Text[] expTextArray;
+	public Slider[] expSlider;
+
+	public GameObject[] charStatHolder;
+	private CharStats[] playerStats;
 
 	// Start is called before the first frame update
 	void Start()
@@ -17,15 +31,67 @@ public class GameMenu : MonoBehaviour
 		{
 			if (theMenu.activeInHierarchy)
 			{
-				theMenu.SetActive(false);
-				GameManager.instense.gameMenuOpen = false;
-			} 
+				CloseMenu();
+			}
 			else
 			{
 				theMenu.SetActive(true);
+				UpdateMainStats();
 				GameManager.instense.gameMenuOpen = true;
 			}
 		}
+	}
 
+	public void UpdateMainStats()
+	{
+		playerStats = GameManager.instense.playerStats;
+
+		for (int i = 0; i < playerStats.Length; i++)
+		{
+			if (playerStats[i].gameObject.activeInHierarchy)
+			{
+				charStatHolder[i].SetActive(true);
+
+				nameTextArray[i].text = playerStats[i].nameHero;
+				hpTextArray[i].text = "HP: " + playerStats[i].currentHP + "/" + playerStats[i].maxHP;
+				mpTextArray[i].text = "MP: " + playerStats[i].currentMP + "/" +  playerStats[i].maxMP;
+				lvlTextArray[i].text = "Level: " + playerStats[i].playerLevel;
+				expTextArray[i].text = "" + playerStats[i].currentExp + "/" + playerStats[i].expToNextLevel[playerStats[i].playerLevel];
+				expSlider[i].maxValue = playerStats[i].expToNextLevel[playerStats[i].playerLevel];
+				expSlider[i].value = playerStats[i].currentExp;
+				personalImage[i].sprite = playerStats[i].charImage;
+			}
+			else
+			{
+				charStatHolder[i].SetActive(false);
+			}
+		}
+	}
+
+	public void ToggleWindow(int windowNumber)
+	{
+		for (int i = 0; i < windows.Length; i++)
+		{
+			if (i == windowNumber)
+			{
+				windows[i].SetActive(!windows[i].activeInHierarchy);
+			}
+			else
+			{
+				windows[i].SetActive(false);
+			}
+		}
+	}
+
+	public void CloseMenu()
+	{
+		for (int i = 0; i < windows.Length; i++)
+		{
+			windows[i].SetActive(false);
+		}
+
+		theMenu.SetActive(false);
+
+		GameManager.instense.gameMenuOpen = false;
 	}
 }
