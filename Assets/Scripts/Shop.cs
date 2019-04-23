345782e2e765da13cc2d2c36b0f4f9cb9c2cffeb
AlexanderPK <alexanderpodvisotskyi@gmail.com>
2019-04-23 23:19:08 +0300

    Work with Buy and Sell items

--- Assets/Scripts/Shop.cs
+++ Assets/Scripts/Shop.cs
@@ -12,7 +12,10 @@ public class Shop : MonoBehaviour
 
 	public Text goldText;
 
-	public string[] ItemsForSale;
+	public string[] ItemsForSale = new string[60];
+
+	public ItemButton[] buyItemButtonsArray;
+	public ItemButton[] sellItemButtonsArray;
 
 	// Start is called before the first frame update
 	void Start()
@@ -50,11 +53,49 @@ public class Shop : MonoBehaviour
 	{
 		buyMenu.SetActive(true);
 		sellMenu.SetActive(false);
+
+		GameManager.instense.SortItems();
+
+		for (int i = 0; i < buyItemButtonsArray.Length; i++)
+		{
+			buyItemButtonsArray[i].buttonValue = i;
+
+			if (ItemsForSale[i] != "")
+			{
+				buyItemButtonsArray[i].buttonImage.gameObject.SetActive(true);
+				buyItemButtonsArray[i].buttonImage.sprite = GameManager.instense.GetItemDetails(ItemsForSale[i]).itemSprite;
+				buyItemButtonsArray[i].amountText.text = "";
+			}
+			else
+			{
+				buyItemButtonsArray[i].buttonImage.gameObject.SetActive(false);
+				buyItemButtonsArray[i].amountText.text = "";
+			}
+		}
 	}
 
 	public void OpenSellMenu()
 	{
 		sellMenu.SetActive(true);
 		buyMenu.SetActive(false);
+
+		GameManager.instense.SortItems();
+
+		for (int i = 0; i < sellItemButtonsArray.Length; i++)
+		{
+			sellItemButtonsArray[i].buttonValue = i;
+
+			if (GameManager.instense.itemHeldArray[i] != "")
+			{
+				sellItemButtonsArray[i].buttonImage.gameObject.SetActive(true);
+				sellItemButtonsArray[i].buttonImage.sprite = GameManager.instense.GetItemDetails(GameManager.instense.itemHeldArray[i]).itemSprite;
+				sellItemButtonsArray[i].amountText.text = "";
+			}
+			else
+			{
+				sellItemButtonsArray[i].buttonImage.gameObject.SetActive(false);
+				sellItemButtonsArray[i].amountText.text = "";
+			}
+		}
 	}
 }
