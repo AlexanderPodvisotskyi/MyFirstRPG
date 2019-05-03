4f5a2c06140ac7d2e17a807f699924eb539705d8
AlexanderPK <alexanderpodvisotskyi@gmail.com>
2019-05-03 20:52:13 +0300

    Create new enemy

---
+++ Assets/Scripts/BattleCharacter.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class BattleCharacter : MonoBehaviour
+{
+	public bool isPlayer;
+	public string[] movesAvailable;
+
+	public string characterName;
+
+	public int currentHP;
+	public int maxHP;
+
+	public int currentMP;
+	public int maxMP;
+
+	public int strength;
+	public int defence;
+
+	public int weaponPower;
+	public int armorPower;
+
+	public bool hasDied;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+}
