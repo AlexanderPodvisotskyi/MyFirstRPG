94339e286ffb68b2b5216df0aa0a6373df248727
AlexanderPK <alexanderpodvisotskyi@gmail.com>
2019-05-09 14:51:13 +0300

    Add attack enemies

--- Assets/Scripts/BattleManager.cs
+++ Assets/Scripts/BattleManager.cs
@@ -35,6 +35,9 @@ public class BattleManager : MonoBehaviour
 	public Text[] playerHP;
 	public Text[] playerMP;
 
+	public GameObject targetMenu;
+	public BattleTargetsButton[] targetButtons;
+
 	// Start is called before the first frame update
 	void Start()
 	{
@@ -295,9 +298,8 @@ public class BattleManager : MonoBehaviour
 		}
 	}
 
-	public void PlayerAttack(string moveName /*, int selectedTarget */)
+	public void PlayerAttack(string moveName, int selectedTarget)
 	{
-		int selectedTarget = 2;
 
 		int movePower = 0;
 
@@ -315,6 +317,39 @@ public class BattleManager : MonoBehaviour
 		DealDamage(selectedTarget, movePower);
 
 		uiButtonsHolder.SetActive(false);
+		targetMenu.SetActive(false);
+
 		NextTurn();
+	}
+
+	public void OpenTargetMenu(string moveName)
+	{
+		targetMenu.SetActive(true);
+
+		List<int> Enemies = new List<int>();
+
+		for (int i = 0; i < activeBattlers.Count; i++)
+		{
+			if (!activeBattlers[i].isPlayer)
+			{
+				Enemies.Add(i);
+			}
+		}
+
+		for (int i = 0; i < targetButtons.Length; i++)
+		{
+			if (Enemies.Count > i)
+			{
+				targetButtons[i].gameObject.SetActive(true);
+
+				targetButtons[i].moveName = moveName;
+				targetButtons[i].activeBattlerTarget = Enemies[i];
+				targetButtons[i].targetName.text = activeBattlers[Enemies[i]].characterName;
+			}
+			else
+			{
+				targetButtons[i].gameObject.SetActive(false);
+			}
+		}
 	}
 }
---
+++ Assets/Scripts/BattleTargetsButton.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class BattleTargetsButton : MonoBehaviour
+{
+	public string moveName;
+	public int activeBattlerTarget;
+	public Text targetName;
+
+
+
+	// Start is called before the first frame update
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	public void Press()
+	{
+		BattleManager.instance.PlayerAttack(moveName, activeBattlerTarget);
+			
+	}
+}
